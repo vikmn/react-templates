2fdf7b91331bb935f052eb2a945aac787163f334
Vikram Nadig <Vikram.Nadig@comparisontech.com>
2019-06-26 14:58:19 +0100

    setting up the signalR server side implementation for dotnetify

---
+++ HelloWorld.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading;
+using DotNetify;
+
+namespace react_template_core
+{
+    public class HelloWorld : BaseVM
+    {
+        private readonly Timer _timer;
+        public string Greetings => "Hello World!";
+        public DateTime ServerTime => DateTime.Now;
+
+        public HelloWorld()
+        {
+            _timer = new Timer(state =>
+            {
+                Changed(nameof(ServerTime));
+                PushUpdates();
+            }, null, 0, 1000);
+        }
+
+        public override void Dispose() => _timer.Dispose();
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetify;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,11 +16,24 @@ namespace react_template_core
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+            services.AddMemoryCache();
+            services.AddSignalR();
+            services.AddDotNetify(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(builder => builder
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .WithOrigins("http://localhost:3000")
+                .AllowCredentials());
+
+            app.UseWebSockets();
+            app.UseSignalR(routes => routes.MapDotNetifyHub());
+            app.UseDotNetify();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
